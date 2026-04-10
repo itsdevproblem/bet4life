907114eac04793b4b1d07e9c86f0f9272ccf2c34
dooyounglee <gostbaducking1@nate.com>
2026-04-10 19:15:52 +0900

    fix: @QueryProjection 주석침

--- src/main/java/com/example/betting/user/dto/UserSimpleDto.java
+++ src/main/java/com/example/betting/user/dto/UserSimpleDto.java
@@ -1,7 +1,5 @@
 package com.example.betting.user.dto;
 
-import com.querydsl.core.annotations.QueryProjection;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,7 +9,7 @@ public class UserSimpleDto {
     private Long id;
     private Integer age;
 
-    @QueryProjection // 빌드하면 QUserSimpleDto가 생성되어 더 안전하게 조회 가능 (선택사항)
+    // @QueryProjection // 빌드하면 QUserSimpleDto가 생성되어 더 안전하게 조회 가능 (선택사항)
     public UserSimpleDto(Long id, Integer age) {
         this.id = id;
         this.age = age;
